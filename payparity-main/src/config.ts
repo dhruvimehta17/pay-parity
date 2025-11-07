2b5055288ddbf4afb1f47e30ecd22ee94ae81860
warp <warp@Dhruvis-MacBook-Air-2.local>
2025-11-07 16:29:38 +0530

    frontend changes

--- payparity-main/src/config.ts
+++ payparity-main/src/config.ts
@@ -1,3 +1,4 @@
 // src/config.ts
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const BACKEND_URL: string =
+  (import.meta.env.VITE_BACKEND_URL as string) || "http://localhost:8000";
 
